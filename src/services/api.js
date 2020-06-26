fedd1625edf7627482993c1adb78d5625db866ea
tonnyrogers <antoniel15975@gmail.com>
2020-06-26 15:28:55 -0300

    API Headers Update

--- src/services/api.js
+++ src/services/api.js
@@ -2,6 +2,9 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://34.72.241.75:3333',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
