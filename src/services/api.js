f9199800953cc13e6b762fa4061398eccbb40fdd
tonnyrogers <antoniel15975@gmail.com>
2020-08-21 10:53:05 -0300

    [fix] Allow Origin

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://34.72.241.75:3333',
+  baseURL: 'http://34.72.241.75',
   headers: {
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
   },
 });
 
